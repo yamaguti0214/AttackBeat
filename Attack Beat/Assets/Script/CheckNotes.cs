2024b10b69ed908ecad35bd56989179a9e5d3f31
yamagutitokinari <c241047@ivy.ac.jp>
2026-04-17 12:01:27 +0900

    HIt時のエフェクトと判定 フォント

--- Attack Beat/Assets/Script/CheckNotes.cs
+++ Attack Beat/Assets/Script/CheckNotes.cs
@@ -1,15 +1,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UIElements;
+using System.Collections;
 
 public class CheckNotes : MonoBehaviour
 {
     [System.Serializable]
     public class Note
     {
+        public GameObject Notes;
         public float timing;
         public bool isHit;
     }
+    
+    //ノーツの判定結果
+    [SerializeField] public TextMeshProUGUI Perfecttxt;
+    [SerializeField] public TextMeshProUGUI Greatttxt;
+    [SerializeField] public TextMeshProUGUI Goodtxt;
+    private int Perfect;
+    private int Great;
+    private int Good;
+
+    private int DestoryNotes = 0;
+
+    //Effect
+    [SerializeField] public GameObject PerfectEffect;
+    [SerializeField] public GameObject GreatEffect;
+    [SerializeField] public GameObject GoodEffect;
+
+    [SerializeField]public Vector2 CheckPosition;
 
     SoundPlay SoundPlay;
 
@@ -18,8 +38,8 @@ public class CheckNotes : MonoBehaviour
 
     public TextMeshProUGUI resultText; // ← ここにセット
 
-    public float perfectRange = 0.05f;
-    public float greatRange = 0.1f;
+    public float perfectRange = 0.1f;
+    public float greatRange = 0.15f;
     public float goodRange = 0.2f;
 
     void Update()
@@ -58,17 +78,25 @@ public class CheckNotes : MonoBehaviour
         if (closestDiff <= perfectRange)
         {
             closestNote.isHit = true;
+            Perfect++;
             ShowResult("Perfect");
+            NotesEffect("Perfect");
+            DestoryNotes++;
+            //Destory(notes);
         }
         else if (closestDiff <= greatRange)
         {
             closestNote.isHit = true;
+            Great++;
             ShowResult("Great");
+            NotesEffect("Great");
         }
         else if (closestDiff <= goodRange)
         {
             closestNote.isHit = true;
+            Good++;
             ShowResult("Good");
+            NotesEffect("Good");
         }
         else
         {
@@ -97,6 +125,37 @@ public class CheckNotes : MonoBehaviour
         if (resultText != null)
         {
             resultText.text = result;
+        }
+
+        switch (result)
+        {
+            case "Perfect":
+                Perfecttxt.text = "Perfect : " + Perfect;
+                break;
+            case "Great":
+                Greatttxt.text = "Great : " + Great;
+                break;
+            case "Good":
+                Goodtxt.text = "Good : " + Good;
+                break;
+
+        }
+
+    }
+
+    void NotesEffect(string Note_Check)
+    {
+        switch(Note_Check)
+        {
+            case "Perfect":
+                Instantiate(PerfectEffect, CheckPosition, Quaternion.identity);
+                break;
+            case "Great":
+                Instantiate(GreatEffect, CheckPosition, Quaternion.identity);
+                break;
+            case "Good":
+                Instantiate(GoodEffect, CheckPosition, Quaternion.identity);
+                break;
         }
     }
 }
