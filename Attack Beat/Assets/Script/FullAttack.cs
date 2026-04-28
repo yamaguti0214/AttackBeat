693cd0f57becffb230f9e9daee53f6003819acd2
yamagutitokinari <c241047@ivy.ac.jp>
2026-04-28 11:14:23 +0900

    Gerge

--- Attack Beat/Assets/Script/FullAttack.cs
+++ Attack Beat/Assets/Script/FullAttack.cs
@@ -7,10 +7,36 @@ public class FullAttack : MonoBehaviour
 {
     EnemyHP enemyHP;
     [SerializeField] private Image[] AttackgergeImage;
+
+    
+    Color[] rainbow;            //ゲージマックスの虹色の配列
+    private bool GergeMAX = false;
+
+    private float GergeTimer = 0f;
+    private int CurrenMoveColor = 0;
     // Start is called before the first frame update
     void Start()
     {
         enemyHP = FindFirstObjectByType<EnemyHP>();
+
+        rainbow = new Color[15]
+        {
+            new Color32(255,0,0,255),
+            new Color32(255,64,0,255),
+            new Color32(255,128,0,255),
+            new Color32(255,191,0,255),
+            new Color32(255,255,0,255),
+            new Color32(191,255,0,255),
+            new Color32(128,255,0,255),
+            new Color32(64,255,0,255),
+            new Color32(0,255,0,255),
+            new Color32(0,255,128,255),
+            new Color32(0,255,255,255),
+            new Color32(0,128,255,255),
+            new Color32(0,0,255,255),
+            new Color32(128,0,255,255),
+            new Color32(255,0,255,255)
+        };
     }
 
     // Update is called once per frame
@@ -41,6 +67,23 @@ public class FullAttack : MonoBehaviour
             gergeImageColor.a = 1f;
             gergeImageColor = new Color(0f, 1f, 0f);
             AttackgergeImage[2].color = gergeImageColor;
+        }
+
+        if(GergeMAX)
+        {
+            GergeTimer += Time.deltaTime;
+            if(GergeTimer >= 0.5f)
+            {
+                GergeTimer = 0;
+                for(int i = 0;i < 15;i++)
+                {
+                    CurrenMoveColor++;
+                    if (i + CurrenMoveColor >= 15)
+                    {
+                        
+                    }
+                }
+            }
         }
     }
 
