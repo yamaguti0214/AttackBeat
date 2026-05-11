d274f429dafe4ea17bfe628e82ecd45329c2a3aa
yamagutitokinari <c241047@ivy.ac.jp>
2026-05-11 10:41:49 +0900

    AttackEffect

--- Attack Beat/Assets/Script/FullAttack.cs
+++ Attack Beat/Assets/Script/FullAttack.cs
@@ -74,36 +74,117 @@ public class FullAttack : MonoBehaviour
 
         if(!GergeMAX)
         {
-            if (CheckNotes.FullAttack >= 5 && AttackgergeImage[0].color.a < 1)
+            if (CheckNotes.FullAttack >= OverAttack / 15 && AttackgergeImage[0].color.a < 1)
             {
                 Color gergeImageColor = AttackgergeImage[0].color;
                 gergeImageColor.a = 1f;
                 gergeImageColor = new Color(0f, 1f, 0f);
                 AttackgergeImage[0].color = gergeImageColor;
             }
-            else if (CheckNotes.FullAttack >= 10 && AttackgergeImage[1].color.a < 1)
+            else if (CheckNotes.FullAttack >= OverAttack / 15 * 2&& AttackgergeImage[1].color.a < 1)
             {
                 Color gergeImageColor = AttackgergeImage[1].color;
                 gergeImageColor.a = 1f;
                 gergeImageColor = new Color(0f, 1f, 0f);
                 AttackgergeImage[1].color = gergeImageColor;
             }
-            else if (CheckNotes.FullAttack >= 15 && AttackgergeImage[2].color.a < 1)
+            else if (CheckNotes.FullAttack >= OverAttack / 15 * 3 && AttackgergeImage[2].color.a < 1)
             {
                 Color gergeImageColor = AttackgergeImage[2].color;
                 gergeImageColor.a = 1f;
                 gergeImageColor = new Color(0f, 1f, 0f);
                 AttackgergeImage[2].color = gergeImageColor;
             }
-
-            if(CheckNotes.FullAttack >= 20)
+            else if (CheckNotes.FullAttack >= OverAttack / 15 * 4&& AttackgergeImage[3].color.a < 1)
             {
+                Color gergeImageColor = AttackgergeImage[3].color;
+                gergeImageColor.a = 1f;
+                gergeImageColor = new Color(0f, 1f, 0f);
+                AttackgergeImage[3].color = gergeImageColor;
+            }
+            else if (CheckNotes.FullAttack >= OverAttack / 15 * 5 && AttackgergeImage[4].color.a < 1)
+            {
+                Color gergeImageColor = AttackgergeImage[4].color;
+                gergeImageColor.a = 1f;
+                gergeImageColor = new Color(0f, 1f, 0f);
+                AttackgergeImage[4].color = gergeImageColor;
+            }
+            else if (CheckNotes.FullAttack >= OverAttack / 15 * 6 && AttackgergeImage[5].color.a < 1)
+            {
+                Color gergeImageColor = AttackgergeImage[5].color;
+                gergeImageColor.a = 1f;
+                gergeImageColor = new Color(0f, 1f, 0f);
+                AttackgergeImage[5].color = gergeImageColor;
+            }
+            else if (CheckNotes.FullAttack >= OverAttack / 15 * 7&& AttackgergeImage[6].color.a < 1)
+            {
+                Color gergeImageColor = AttackgergeImage[6].color;
+                gergeImageColor.a = 1f;
+                gergeImageColor = new Color(0f, 1f, 0f);
+                AttackgergeImage[6].color = gergeImageColor;
+            }
+            else if (CheckNotes.FullAttack >= OverAttack / 15 * 8 && AttackgergeImage[7].color.a < 1)
+            {
+                Color gergeImageColor = AttackgergeImage[7].color;
+                gergeImageColor.a = 1f;
+                gergeImageColor = new Color(0f, 1f, 0f);
+                AttackgergeImage[7].color = gergeImageColor;
+            }
+            else if (CheckNotes.FullAttack >= OverAttack / 15 * 9 && AttackgergeImage[8].color.a < 1)
+            {
+                Color gergeImageColor = AttackgergeImage[8].color;
+                gergeImageColor.a = 1f;
+                gergeImageColor = new Color(0f, 1f, 0f);
+                AttackgergeImage[8].color = gergeImageColor;
+            }
+            else if (CheckNotes.FullAttack >= OverAttack / 15 * 10&& AttackgergeImage[9].color.a < 1)
+            {
+                Color gergeImageColor = AttackgergeImage[9].color;
+                gergeImageColor.a = 1f;
+                gergeImageColor = new Color(0f, 1f, 0f);
+                AttackgergeImage[9].color = gergeImageColor;
+            }
+            else if (CheckNotes.FullAttack >= OverAttack / 15 * 11 && AttackgergeImage[10].color.a < 1)
+            {
+                Color gergeImageColor = AttackgergeImage[10].color;
+                gergeImageColor.a = 1f;
+                gergeImageColor = new Color(0f, 1f, 0f);
+                AttackgergeImage[10].color = gergeImageColor;
+            }
+            else if (CheckNotes.FullAttack >= OverAttack / 15 * 12 && AttackgergeImage[11].color.a < 1)
+            {
+                Color gergeImageColor = AttackgergeImage[11].color;
+                gergeImageColor.a = 1f;
+                gergeImageColor = new Color(0f, 1f, 0f);
+                AttackgergeImage[11].color = gergeImageColor;
+            }
+            else if (CheckNotes.FullAttack >= OverAttack / 15 * 13 && AttackgergeImage[12].color.a < 1)
+            {
+                Color gergeImageColor = AttackgergeImage[12].color;
+                gergeImageColor.a = 1f;
+                gergeImageColor = new Color(0f, 1f, 0f);
+                AttackgergeImage[12].color = gergeImageColor;
+            }
+            else if (CheckNotes.FullAttack >= OverAttack / 15 * 14 && AttackgergeImage[13].color.a < 1)
+            {
+                Color gergeImageColor = AttackgergeImage[13].color;
+                gergeImageColor.a = 1f;
+                gergeImageColor = new Color(0f, 1f, 0f);
+                AttackgergeImage[13].color = gergeImageColor;
+            }
+            else if (CheckNotes.FullAttack >= OverAttack / 15 * 15 && AttackgergeImage[14].color.a < 1)
+            {
+                Color gergeImageColor = AttackgergeImage[14].color;
+                gergeImageColor.a = 1f;
+                gergeImageColor = new Color(0f, 1f, 0f);
+                AttackgergeImage[14].color = gergeImageColor;
                 GergeMAX = true;
                 MAXTextObject.SetActive(true);
             }
         }
         else if (GergeMAX)
         {
+            //ゲージをレインボーにする
             GergeTimer += Time.deltaTime;
             if(GergeTimer >= 0.04f)
             {
@@ -133,26 +214,44 @@ public class FullAttack : MonoBehaviour
         {
             Attack = true;
 
-            switch (CheckNotes.FullAttack)
+            switch (CheckNotes.FullAttack + 50)
             {
                 case int n when n >= OverAttack:
+                    CreateEffect(new Color(1f, 0f, 1f));
+                    CreateEffect(Color.red);
                     CreateEffect(Color.blue);
+                    CreateEffect(Color.green);
+                    CreateEffect(Color.gray);
+                    Attack = true;
                     break;
                 case int n when n >= MaxAttack:
+                    CreateEffect(Color.red);
+                    CreateEffect(Color.blue);
+                    CreateEffect(Color.green);
+                    CreateEffect(Color.white);
                     Attack = true;
                     break;
                 case int n when n >= BigAttack:
+                    CreateEffect(Color.red);
+                    CreateEffect(Color.blue);
+                    CreateEffect(Color.green);
+                    CreateEffect(Color.white);
                     Attack = false;
                     break;
                 case int n when n >= MediumAttack:
+                    CreateEffect(Color.blue);
+                    CreateEffect(Color.green);
+                    CreateEffect(Color.white);
                     Attack = true;
                     break;
                 case int n when n >= SmallAttack:
+                    CreateEffect(Color.green);
+                    CreateEffect(Color.white);
                     Attack = true;
                     break;
                 default:
-                    Debug.Log("REEEEED");
-                    CreateEffect(Color.green);
+                    CreateEffect(Color.white);
+                    Attack = true;
                     break;
             }
 
