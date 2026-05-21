78bcb9db9a28ccb6e8d046f01e6ca2eb2e0bfc82
SatoHaru <c241021@ivy.ac.jp>
2026-05-21 11:27:30 +0900

    add scenes and audio

---
+++ Attack Beat/Assets/Script/GameOver.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public static bool Death = false;
+    public static bool Win = false;
+
+    [SerializeField] private TextMeshProUGUI GAMEOVER;
+    [SerializeField] private GameObject ResultButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Death)
+        {
+            Debug.Log("Death");
+            GAMEOVER.text = "GAME OVER";
+            ResultButton.SetActive(true);
+        }
+        else if(Win)
+        {
+            Debug.Log("Win");
+            GAMEOVER.text = "GAME CLEAR";
+            ResultButton.SetActive(true);
+        }
+    }
+}
